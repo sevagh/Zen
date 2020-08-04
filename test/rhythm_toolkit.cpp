a1a64364c709c912ef919686db773b1da0cabd3a
Sevag Hanssian <sevagh@protonmail.com>
2020-08-04 16:51:57 -0400

    Verify IPP

--- test/rhythm_toolkit.cpp
+++ test/rhythm_toolkit.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <rhythm_toolkit/hpss.h>
 #include <rhythm_toolkit/rhythm_toolkit.h>
+#include <opencv2/core/utility.hpp>
 
 int main(int argc, char** argv)
 {
 	rhythm_toolkit::hello_world();
 	auto hpss = rhythm_toolkit::hpss::HPSS();
 	hpss.do_work();
+	std::cout << cv::getBuildInformation() << std::endl;
 	return 0;
 }
